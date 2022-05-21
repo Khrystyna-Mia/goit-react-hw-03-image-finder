bbcba3253c497f78f3badcf5873e59cb6b9806ea
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-05-22 01:50:57 +0300

    refactoring

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,35 +3,15 @@ import axios from 'axios';
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '25738205-42a7db76025be689580da6194';
 
-class ImagesApiService {
-  constructor() {
-    this.query = '';
-    this.page = 1;
-  }
-
-  async fetchImg() {
-    const response = await axios.get(
-      `${API_URL}?q=${this.query}&page=${this.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+const ImagesApiService = async (searchQuery, currentPage) => {
+  try {
+    const response = await axios(
+      `${API_URL}?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
-    // this.incrementPage();
     return response.data.hits;
+  } catch (error) {
+    console.log(error);
   }
-
-  get searchQuery() {
-    return this.query;
-  }
-
-  set searchQuery(newQuery) {
-    this.query = newQuery;
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  // resetPage() {
-  //   this.page = 1;
-  // }
-}
+};
 
 export default ImagesApiService;
